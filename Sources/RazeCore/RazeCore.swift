94bf824df1cb1c3ec32e97c8e6b442fdb6d4e402
Michel Torres <mtorres.alon@gmail.com>
2021-11-26 18:22:10 -0600

    Providing static variable for a color palette.

--- Sources/RazeCore/RazeCore.swift
+++ Sources/RazeCore/RazeCore.swift
@@ -8,7 +8,7 @@ public class RazeCore {
     ///   - hexString: A 6-digit hexadecimal string. Use 6 digits rather than 8, and add the accompanying alpha value in the second parameter.
     ///   - alpha: A number between 0.0 and 1.0 indicating how transparent the color is
     /// - Returns: a UIColor defined by the `hexString`parameter
-    class func colorFromHexString(_ hexString: String, alpha: CGFloat = 1.0) -> UIColor {
+    internal class func colorFromHexString(_ hexString: String, alpha: CGFloat = 1.0) -> UIColor {
         let r, g, b: CGFloat
         let offset = hexString.hasPrefix("#") ? 1 : 0
         let start = hexString.index(hexString.startIndex, offsetBy: offset)
@@ -22,5 +22,10 @@ public class RazeCore {
             return UIColor(red: r, green: g, blue: b, alpha: alpha)
         }
         return UIColor(red: 0, green: 0, blue: 0, alpha: alpha)
+    }
+    
+    /// The most eye-preasing color know to all humanity
+    static var razeColor: UIColor {
+        return self.colorFromHexString("006736")
     }
 }
