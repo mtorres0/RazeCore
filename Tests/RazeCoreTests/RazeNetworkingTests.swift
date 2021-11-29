57cf64abc21dba4d3949304229f63fa55682721e
Michel Torres <mtorres.alon@gmail.com>
2021-11-29 17:44:08 -0600

    Added second Networking API for sending data.

--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -15,6 +15,15 @@ class NetworkSessionMock: NetworkSession {
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
         completionHandler(data, error)
     }
+    
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
+}
+
+struct MockData: Codable, Equatable {
+    var id: Int
+    var name: String
 }
 
 final class RazeNetworkingTests: XCTestCase {
@@ -32,6 +41,28 @@ final class RazeNetworkingTests: XCTestCase {
             switch result {
                 case .success(let returnedData):
                 XCTAssertEqual(data, returnedData, "Response data is nil")
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    func testSendDataCall() {
+        let manager = RazeCore.Networking.Manager()
+        let session = NetworkSessionMock()
+        let sampleObject = MockData(id: 1, name: "David")
+        let data = try? JSONEncoder().encode(sampleObject)
+        session.data = data
+        manager.session = session
+        let url = URL(fileURLWithPath: "url")
+        let expectation = XCTestExpectation(description: "Send data")
+        manager.sendData(to: url, body: sampleObject) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                let returnedObject = try? JSONDecoder().decode(MockData.self, from: returnedData)
+                XCTAssertEqual(returnedObject, sampleObject)
             case .failure(let error):
                 XCTFail(error?.localizedDescription ?? "error forming error result")
             }
